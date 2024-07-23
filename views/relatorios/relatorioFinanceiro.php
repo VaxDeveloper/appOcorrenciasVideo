7f3c9f8e21c8e841bffe689e5d4b5b025c88e6a4
Victor Xavier <148595747+VaxDeveloper@users.noreply.github.com>
2024-07-23 09:56:42 -0300

    Atualizando relatório

--- views/relatorios/relatorioFinanceiro.php
+++ views/relatorios/relatorioFinanceiro.php
@@ -20,10 +20,10 @@
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link" href="../ocorrenciasVideos.php"><i class="fas fa-table"></i><span>Ocorrências</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="./relatorioVideo.html"><i class="fas fa-tachometer-alt"></i><span>Vídeo</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="./relatorioFinanceiro.php"><i class="fas fa-tachometer-alt"></i><span>Financeiro</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="./relatorioOp.php"><i class="fas fa-tachometer-alt"></i><span>Operacional</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="ocorrenciasVideos.php"><i class="fas fa-table"></i><span>Ocorrências</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="relatorios/relatorioVideo.html"><i class="fas fa-tachometer-alt"></i><span>Vídeo</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="relatorios/relatorioFinanceiro.php"><i class="fas fa-tachometer-alt"></i><span>Financeiro</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="relatorios/relatorioOp.php"><i class="fas fa-tachometer-alt"></i><span>Operacional</span></a></li>
                     <li class="nav-item"><a class="nav-link"><i class="fas fa-user"></i><span>Profile</span></a></li>
                     <li class="nav-item"><a class="nav-link"><i class="fas fa-user-circle"></i><span>Register</span></a></li>
                 </ul>
@@ -63,7 +63,7 @@
                                     <div class="row g-0 align-items-center">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-primary fw-bold text-xs mb-1"><span>Pag. Acumulado (julho)</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 295.720,40&nbsp;</span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 350.506,40&nbsp;</span></div>
                                         </div>
                                         <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
                                     </div>
@@ -76,7 +76,7 @@
                                     <div class="row g-0 align-items-center">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-success fw-bold text-xs mb-1"><span>VT. Acumulado (julho)</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 433.887,75&nbsp;</span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 515.551,50&nbsp;</span></div>
                                         </div>
                                         <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
                                     </div>
@@ -111,7 +111,7 @@
                                     <div class="row g-0 align-items-center">
                                         <div class="col me-2">
                                             <div class="text-uppercase text-warning fw-bold text-xs mb-1"><span>Total receita</span></div>
-                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 984.669,60&nbsp;</span></div>
+                                            <div class="text-dark fw-bold h5 mb-0"><span>&nbsp;R$ 866.057,90&nbsp;</span></div>
                                         </div>
                                         <div class="col-auto"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i></div>
                                     </div>
@@ -132,7 +132,7 @@
                             <div class="card">
                                 <div class="card-body">
                                     <h4 class="card-title">Pag. Acumulado - SM x VM (Julho)</h4>
-                                    <div><canvas data-bss-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Pagantes SM&quot;,&quot;Pagantes VM&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;rgb(78,115,223)&quot;,&quot;rgb(255,134,134)&quot;],&quot;borderColor&quot;:[&quot;rgba(0,0,0,0.1)&quot;,&quot;rgba(0,0,0,0.1)&quot;],&quot;data&quot;:[&quot;29572040 &quot;,&quot;36039360 &quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;},&quot;position&quot;:&quot;right&quot;},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;bold&quot;,&quot;display&quot;:false,&quot;text&quot;:&quot;&quot;}}}"></canvas></div>
+                                    <div><canvas data-bss-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Pagantes SM&quot;,&quot;Pagantes VM&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;rgb(78,115,223)&quot;,&quot;rgb(255,134,134)&quot;],&quot;borderColor&quot;:[&quot;rgba(0,0,0,0.1)&quot;,&quot;rgba(0,0,0,0.1)&quot;],&quot;data&quot;:[&quot;35050640 &quot;,&quot;42814080 &quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;},&quot;position&quot;:&quot;right&quot;},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;bold&quot;,&quot;display&quot;:false,&quot;text&quot;:&quot;&quot;}}}"></canvas></div>
                                 </div>
                             </div>
                         </div>
@@ -142,7 +142,7 @@
                             <div class="card">
                                 <div class="card-body">
                                     <h4 class="card-title">Acompanhamento diário - Pagantes SM x VM (Julho)</h4>
-                                    <div><canvas data-bss-chart="{&quot;type&quot;:&quot;bar&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;01/07/2024&quot;,&quot;02/07/2024&quot;,&quot;03/07/2024&quot;,&quot;04/07/2024&quot;,&quot;05/07/2024&quot;,&quot;06/07/2022&quot;,&quot;07/07/2024&quot;,&quot;08/07/2024&quot;,&quot;09/07/2024&quot;,&quot;10/07/2024&quot;,&quot;11/07/2004&quot;,&quot;12/07/2024&quot;,&quot;13/07/2024&quot;,&quot;14/07/2022&quot;,&quot;15/07/2024&quot;,&quot;16/07/2024&quot;,&quot;17/07/2024&quot;,&quot;18/07/2024&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;SM&quot;,&quot;backgroundColor&quot;:&quot;#4e73df&quot;,&quot;borderColor&quot;:&quot;#4e73df&quot;,&quot;data&quot;:[&quot; 2040480 &quot;,&quot; 517920 &quot;,&quot; 2393760 &quot;,&quot; 2055840 &quot;,&quot;2215200&quot;,&quot; 1328640 &quot;,&quot; 461280 &quot;,&quot; 2248320 &quot;,&quot; 2008320 &quot;,&quot; 1937360 &quot;,&quot;1853279&quot;,&quot; 1787040 &quot;,&quot; 1278240 &quot;,&quot;518400 &quot;,&quot;1857120\t&quot;,&quot; 1774080 &quot;,&quot; 1630080 &quot;,&quot; 1666680 &quot;]},{&quot;label&quot;:&quot;VM&quot;,&quot;backgroundColor&quot;:&quot;rgb(255,134,134)&quot;,&quot;borderColor&quot;:&quot;rgb(255,134,134)&quot;,&quot;data&quot;:[&quot;2437440 &quot;,&quot;606720 &quot;,&quot;2725440 &quot;,&quot;2366880 &quot;,&quot;2517600 &quot;,&quot;1682880 &quot;,&quot;642240 &quot;,&quot;2673120 &quot;,&quot;2419680 &quot;,&quot;2309280 &quot;,&quot;2216640 &quot;,&quot;2312160 &quot;,&quot;1604160 &quot;,&quot;647520 &quot;,&quot;2395680 &quot;,&quot;2156160 &quot;,&quot;2118240 &quot;,&quot;2207520 &quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;bold&quot;},&quot;scales&quot;:{&quot;xAxes&quot;:[{&quot;ticks&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}],&quot;yAxes&quot;:[{&quot;ticks&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}]}}}"></canvas></div>
+                                    <div><canvas data-bss-chart="{&quot;type&quot;:&quot;bar&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;01/07/2024&quot;,&quot;02/07/2024&quot;,&quot;03/07/2024&quot;,&quot;04/07/2024&quot;,&quot;05/07/2024&quot;,&quot;06/07/2022&quot;,&quot;07/07/2024&quot;,&quot;08/07/2024&quot;,&quot;09/07/2024&quot;,&quot;10/07/2024&quot;,&quot;11/07/2004&quot;,&quot;12/07/2024&quot;,&quot;13/07/2024&quot;,&quot;14/07/2022&quot;,&quot;15/07/2024&quot;,&quot;16/07/2024&quot;,&quot;17/07/2024&quot;,&quot;18/07/2024&quot;,&quot;19/07/2024&quot;,&quot;20/07/2024&quot;,&quot;21/07/2024&quot;,&quot;22/07/2024&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;SM&quot;,&quot;backgroundColor&quot;:&quot;#4e73df&quot;,&quot;borderColor&quot;:&quot;#4e73df&quot;,&quot;data&quot;:[&quot; 2040480 &quot;,&quot; 517920 &quot;,&quot; 2393760 &quot;,&quot; 2055840 &quot;,&quot;2215200&quot;,&quot; 1328640 &quot;,&quot; 461280 &quot;,&quot; 2248320 &quot;,&quot; 2008320 &quot;,&quot; 1937360 &quot;,&quot;1853279&quot;,&quot; 1787040 &quot;,&quot; 1278240 &quot;,&quot;518400 &quot;,&quot;1857120\t&quot;,&quot; 1774080 &quot;,&quot; 1630080 &quot;,&quot; 1672800 &quot;,&quot; 1776480 &quot;,&quot; 1410720 &quot;,&quot; 497760 &quot;,&quot; 1787520 &quot;]},{&quot;label&quot;:&quot;VM&quot;,&quot;backgroundColor&quot;:&quot;rgb(255,134,134)&quot;,&quot;borderColor&quot;:&quot;rgb(255,134,134)&quot;,&quot;data&quot;:[&quot;2437440 &quot;,&quot;606720 &quot;,&quot;2725440 &quot;,&quot;2366880 &quot;,&quot;2517600 &quot;,&quot;1682880 &quot;,&quot;642240 &quot;,&quot;2673120 &quot;,&quot;2419680 &quot;,&quot;2309280 &quot;,&quot;2216640 &quot;,&quot;2312160 &quot;,&quot;1604160 &quot;,&quot;647520 &quot;,&quot;2395680 &quot;,&quot;2156160 &quot;,&quot;2118240 &quot;,&quot;2207520 &quot;,&quot;2242080 &quot;,&quot;1634400 &quot;,&quot;616800 &quot;,&quot;2281440 &quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false,&quot;labels&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}},&quot;title&quot;:{&quot;fontStyle&quot;:&quot;bold&quot;},&quot;scales&quot;:{&quot;xAxes&quot;:[{&quot;ticks&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}],&quot;yAxes&quot;:[{&quot;ticks&quot;:{&quot;fontStyle&quot;:&quot;normal&quot;}}]}}}"></canvas></div>
                                 </div>
                             </div>
                         </div>
@@ -164,8 +164,8 @@
                                         <tr>
                                             <td>01/07/24</td>
                                             <td>20.404,80&nbsp;</td>
-                                            <td>20.223,00&nbsp;</td>
-                                            <td>40.627,80&nbsp;</td>
+                                            <td>20.355,75&nbsp;</td>
+                                            <td>40.760,55&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>02/07/24</td>
@@ -182,8 +182,8 @@
                                         <tr>
                                             <td>04/07/24</td>
                                             <td>20.558,40&nbsp;</td>
-                                            <td>31.311,00&nbsp;</td>
-                                            <td>51.869,40&nbsp;</td>
+                                            <td>31.673,25&nbsp;</td>
+                                            <td>52.231,65&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>05/07/24</td>
@@ -206,8 +206,8 @@
                                         <tr>
                                             <td>08/07/24</td>
                                             <td>22.483,20&nbsp;</td>
-                                            <td>33.108,75&nbsp;</td>
-                                            <td>55.591,95&nbsp;</td>
+                                            <td>33.234,75&nbsp;</td>
+                                            <td>55.717,95&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>09/07/24</td>
@@ -230,14 +230,14 @@
                                         <tr>
                                             <td>12/07/24</td>
                                             <td>17.870,40&nbsp;</td>
-                                            <td>30.766,50&nbsp;</td>
-                                            <td>48.636,90&nbsp;</td>
+                                            <td>30.881,25&nbsp;</td>
+                                            <td>48.751,65&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>13/07/24</td>
                                             <td>12.782,40&nbsp;</td>
-                                            <td>14.463,00&nbsp;</td>
-                                            <td>27.245,40&nbsp;</td>
+                                            <td>14.573,25&nbsp;</td>
+                                            <td>27.355,65&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>14/07/24</td>
@@ -249,7 +249,7 @@
                                             <td>15/07/24</td>
                                             <td>18.571,20</td>
                                             <td>30.033,00</td>
-                                            <td>48.248,70</td>
+                                            <td>48.604,20&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>16/07/24</td>
@@ -260,14 +260,38 @@
                                         <tr>
                                             <td>17/07/24</td>
                                             <td>16.300,80&nbsp;</td>
-                                            <td>26.826,75&nbsp;</td>
-                                            <td>43.127,55&nbsp;</td>
+                                            <td>29.641,50&nbsp;</td>
+                                            <td>45.942,30&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>18/07/24</td>
-                                            <td>16.666,80&nbsp;</td>
-                                            <td>26.752,50&nbsp;</td>
-                                            <td>43.419,30&nbsp;</td>
+                                            <td>16.728,00&nbsp;</td>
+                                            <td>29.344,50&nbsp;</td>
+                                            <td>46.072,50&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>19/07/24</td>
+                                            <td>17.764,80&nbsp;</td>
+                                            <td>28.752,75&nbsp;</td>
+                                            <td>46.517,55&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>20/07/24</td>
+                                            <td>14.107,20&nbsp;</td>
+                                            <td>14.233,50&nbsp;</td>
+                                            <td>28.340,70&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>21/07/24</td>
+                                            <td>4.977,60&nbsp;</td>
+                                            <td>3.777,75&nbsp;</td>
+                                            <td>8.755,35&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>22/07/24</td>
+                                            <td>17.875,20&nbsp;</td>
+                                            <td>28.761,75&nbsp;</td>
+                                            <td>46.636,95&nbsp;</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -355,8 +379,8 @@
                                         <tr>
                                             <td>12/07/24</td>
                                             <td>23.121,60&nbsp;</td>
-                                            <td>45.024,75&nbsp;</td>
-                                            <td>68.146,35&nbsp;</td>
+                                            <td>45.627,75&nbsp;</td>
+                                            <td>68.749,35&nbsp;</td>
                                         </tr>
                                         <tr>
                                             <td>13/07/24</td>
@@ -391,8 +415,32 @@
                                         <tr>
                                             <td>18/07/24</td>
                                             <td>22.075,20&nbsp;</td>
-                                            <td>43.033,50&nbsp;</td>
-                                            <td>65.108,70&nbsp;</td>
+                                            <td>46.145,25&nbsp;</td>
+                                            <td>68.220,45&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>19/07/24</td>
+                                            <td>22.420,80&nbsp;</td>
+                                            <td>45.162,00&nbsp;</td>
+                                            <td>67.582,80&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>20/07/24</td>
+                                            <td>16.344,00&nbsp;</td>
+                                            <td>21.069,00&nbsp;</td>
+                                            <td>37.413,00&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>21/07/24</td>
+                                            <td>6.168,00&nbsp;</td>
+                                            <td>5.751,00&nbsp;</td>
+                                            <td>11.919,00&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td>22/07/24</td>
+                                            <td>22.814,40&nbsp;</td>
+                                            <td>43.677,00&nbsp;</td>
+                                            <td>66.491,40&nbsp;</td>
                                         </tr>
                                     </tbody>
                                 </table>
